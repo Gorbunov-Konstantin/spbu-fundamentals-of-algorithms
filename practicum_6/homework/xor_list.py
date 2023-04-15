eb2d4dce9ece90bc502c9c70bcc306226994fd00
Gorbunov-Konstantin <gorbunovkosty@mail.ru>
2023-04-15 23:07:47 +0300

    xor

--- practicum_6/homework/xor_list.py
+++ practicum_6/homework/xor_list.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from typing import Any
-
+import ctypes
 import yaml
 
 
@@ -9,33 +9,20 @@ import yaml
 class Element:
     key: Any
     data: Any = None
-    np: int = None
+    np: int = 0
 
-    def next(self) -> Element:
+    def next(self, prev_p):
+        return self.np ^ prev_p
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
-
-    def prev(self) -> Element:
-
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
+    def prev(self,next_p):
+        return self.np ^ next_p
 
 
 class XorDoublyLinkedList:
     def __init__(self) -> None:
         self.head: Element = None
-
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
+        self.tail: Element = None
+        self.nodes = []
         pass
 
     def __repr__(self) -> str:
@@ -43,19 +30,22 @@ class XorDoublyLinkedList:
 
     def __str__(self) -> str:
         node_keys = []
-
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
+        next_p = id(self.head)
+        prev_p = 0
+        while next_p != 0:
+            next_el = ctypes.cast(next_p, ctypes.py_object).value
+            node_keys.append(str(next_el.key))
+            prev_p, next_p = next_p, next_el.next(prev_p)
         return " <-> ".join(node_keys)
 
     def to_pylist(self) -> list[Any]:
         py_list = []
-        next_el: Element = self.head()
-        while next_el is not None:
+        next_p = id(self.head)
+        prev_p = 0
+        while next_p != 0:
+            next_el = ctypes.cast(next_p, ctypes.py_object).value
             py_list.append(next_el.key)
-            next_el = next_el.next()
+            prev_p, next_p = next_p, next_el.next(prev_p)
         return py_list
 
     def empty(self):
@@ -63,44 +53,59 @@ class XorDoublyLinkedList:
 
     def search(self, k: Element) -> Element:
         """Complexity: O(n)"""
+        next_p = id(self.head)
+        prev_p = 0
+        while next_p != 0 and next_el.key != k.key:
+            next_el = ctypes.cast(next_p, ctypes.py_object).value
+            prev_p, next_p = next_p, next_el.next(prev_p)
+        return next_el
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
 
     def insert(self, x: Element) -> None:
         """Insert to the front of the list (i.e., it is 'prepend')
         Complexity: O(1)
         """
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
+        if self.head is None:
+            self.head = x
+            self.tail = x
+        else:
+            self.head.np = id(x) ^ self.head.np
+            x.np = id(self.head)
+            self.head = x
+        self.nodes.append(x)
         pass
 
     def remove(self, x: Element) -> None:
         """Remove x from the list
-        Complexity: O(1)
+        Complexity: O(n)
         """
-
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
+        next_p = id(self.head)
+        prev_p = 0
+        next_el = ctypes.cast(next_p, ctypes.py_object).value
+        while next_p != 0 and next_el.key != x.key:
+            prev_p, next_p = next_p, next_el.next(prev_p)
+            next_el = ctypes.cast(next_p, ctypes.py_object).value
+        if next_p != 0:
+            prev_el = ctypes.cast(prev_p, ctypes.py_object).value
+            next_next_p = next_el.np ^ prev_p
+            next_next_el = ctypes.cast(next_next_p, ctypes.py_object).value
+            if prev_p != 0:
+                prev_el.np = prev_el.np ^ next_p ^ next_next_p
+            else:
+                self.head = next_next_el
+            if next_next_p != 0:
+                next_next_el.np = next_next_el.np ^ next_p ^ prev_p
+            else:
+                self.tail = prev_el
+            self.nodes.remove(next_el)
         pass
 
     def reverse(self) -> XorDoublyLinkedList:
         """Returns the same list but in the reserved order
         Complexity: O(1)
         """
-
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
+        self.head, self.tail = self.tail, self.head
+        return self
 
 
 if __name__ == "__main__":
@@ -113,19 +118,20 @@ if __name__ == "__main__":
     # Hint: assuming that self.next and self.prev store pointers
     # define self.np as self.np = self.next XOR self.prev
 
-    with open("practicum_6/homework/xor_list_cases.yaml", "r") as f:
+    with open("xor_list_cases.yaml", "r") as f:
         cases = yaml.safe_load(f)
 
     for i, c in enumerate(cases):
         l = XorDoublyLinkedList()
         for el in reversed(c["input"]["list"]):
-            l.insert(el)
+            l.insert(Element(key=el))
         for op_info in c["input"]["ops"]:
             if op_info["op"] == "insert":
-                l.remove(op_info["key"])
+                l.insert(Element(key=op_info["key"]))
             elif op_info["op"] == "remove":
-                l.remove(op_info["key"])
+                l.remove(Element(key=op_info["key"]))
             elif op_info["op"] == "reverse":
                 l = l.reverse()
         py_list = l.to_pylist()
+        print(py_list)
         print(f"Case #{i + 1}: {py_list == c['output']}")
